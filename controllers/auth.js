99c3de3a4d6c2e99aa32855f14a2c0eec2d01583
Rajat1204 <rajatraghav9719@gmail.com>
2024-03-14 01:26:19 +0530

    Login Functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,8 +44,15 @@ export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email: email });
-    if (!user) {
-    }
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(404).json({ message: 'Invalid Credentials' });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
